8cbc514acb91428f4cb074a9a10db29b0f566e38
Albert Z Wang <albert.zhao.wang@gmail.com>
2017-08-31 12:26:09 -0400

    add FASD_FILTER_FUNCTION to zz

--- functions/zz.fish
+++ functions/zz.fish
@@ -1,3 +1,10 @@
 function zz -d "cd with interactive selection"
-  fasd_cd -di $argv
+  if test (count $FASD_FILTER_FUNCTION) -gt 0
+    set -l dir (fasd -Rdl $argv | eval $FASD_FILTER_FUNCTION)
+    test (count $dir) -gt 0
+    and test -d $dir
+    and builtin cd $dir
+  else
+    fasd_cd -di $argv
+  end
 end
