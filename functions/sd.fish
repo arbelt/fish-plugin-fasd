95dad9b708e0ca20df39745e628d310d1dc58671
Albert Z Wang <albert.zhao.wang@gmail.com>
2017-08-31 13:52:11 -0400

    add filter to sd

--- functions/sd.fish
+++ functions/sd.fish
@@ -1,3 +1,11 @@
+# vim: sw=2
 function sd -d "interactive directory selection"
-  command fasd -sid $argv
+  if test (count $FASD_FILTER_FUNCTION) -gt 0
+    set -l _ret (fasd -Rdl | eval $FASD_FILTER_FUNCTION)
+    test (count $_ret) -eq 1
+    and test -d $_ret
+    and printf "%s" $_ret
+  else
+    command fasd -sid $argv
+  end
 end
