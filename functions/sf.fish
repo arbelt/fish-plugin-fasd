5aa5bd3e9ffa1db738a129dd4861ca9e044e4c1d
Albert Z Wang <albert.zhao.wang@gmail.com>
2017-09-03 14:38:30 -0400

    add filter function support to sf

--- functions/sf.fish
+++ functions/sf.fish
@@ -1,3 +1,10 @@
 function sf -d "interactive file selection"
-  command fasd -sif $argv
+  if test (count $FASD_FILTER_FUNCTION) -gt 0
+    set -l _f (fasd -Rfl $argv | eval $FASD_FILTER_FUNCTION)
+    test (count $_f) -eq 1
+    and test -f $_f
+    and printf "%s" $_f
+  else
+    command fasd -sif $argv
+  end
 end
